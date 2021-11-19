8c8838647a94bf64ef0c8b9ab42fa2b1fa0b92c9
soduma <soduma@naver.com>
2021-11-19 16:28:02 +0900

    ui update

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var toggleButton: UIButton!
+    
+    var duration = 60
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapToggleButton(_ sender: UIButton) {
+        duration = Int(datePicker.countDownDuration)
+        debugPrint(duration)
+    }
 }
 
