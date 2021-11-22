b326ff82e5a3862db0b0be058afd4f0c2b54a164
soduma <soduma@naver.com>
2021-11-22 13:52:12 +0900

    ui logic update

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
 
+class ViewController: UIViewController {
+    
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var progressView: UIProgressView!
     @IBOutlet var datePicker: UIDatePicker!
@@ -16,18 +22,54 @@ class ViewController: UIViewController {
     @IBOutlet var toggleButton: UIButton!
     
     var duration = 60
+    var timerStatus: TimerStatus = .end
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        configureToggleButton()
+    }
+    
     @IBAction func tapCancelButton(_ sender: UIButton) {
+        switch timerStatus {
+        case .start, .pause:
+            timerStatus = .end
+            cancelButton.isEnabled = false
+            setTimerInfoViewVisible(isHidden: true)
+            datePicker.isHidden = false
+            toggleButton.isSelected = false
+        default:
+            break
+        }
     }
     
     @IBAction func tapToggleButton(_ sender: UIButton) {
         duration = Int(datePicker.countDownDuration)
-        debugPrint(duration)
+        
+        switch timerStatus {
+        case .start:
+            timerStatus = .pause
+            toggleButton.isSelected = false
+        case .pause:
+            timerStatus = .start
+            toggleButton.isSelected = true
+        case .end:
+            timerStatus = .start
+            setTimerInfoViewVisible(isHidden: false)
+            datePicker.isHidden = true
+            toggleButton.isSelected = true
+            cancelButton.isEnabled = true
+        }
+    }
+    
+    func setTimerInfoViewVisible(isHidden: Bool) {
+        timerLabel.isHidden = isHidden
+        progressView.isHidden = isHidden
+    }
+    
+    func configureToggleButton() {
+        toggleButton.setTitle("시작", for: .normal)
+        toggleButton.setTitle("일시정지", for: .selected)
     }
 }
 
